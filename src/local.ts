ea1731c062e1991bcc8c5625ecb2a4700237b868
uphg <2633565885@qq.com>
2022-01-26 10:04:29 +0800

    refactor: 重命名 local、session 方法名称

--- src/local.ts
+++ src/local.ts
@@ -2,27 +2,29 @@ import isNil from './_isNil'
 
 const storage = window?.localStorage
 
-export function hasLocal(name: string) {
-  if (isNil(name)) return false
-  return !!storage?.getItem(name)
-}
+export default {
+  has(name: string) {
+    if (isNil(name)) return false
+    return !!storage?.getItem(name)
+  },
 
-export function getLocal(name: string) {
-  if (isNil(name)) return null
-  const result = storage?.getItem(name)
-  return result && JSON.parse(result)
-}
+  get(name: string) {
+    if (isNil(name)) return null
+    const result = storage?.getItem(name)
+    return result && JSON.parse(result)
+  },
 
-export function setLocal(name: string, value: string | number | boolean | Object) {
-  if (isNil(name) || isNil(value)) return
-  return storage?.setItem(name, JSON.stringify(value))
-}
+  set(name: string, value: string | number | boolean | Object) {
+    if (isNil(name) || isNil(value)) return
+    return storage?.setItem(name, JSON.stringify(value))
+  },
 
-export function removeLocal(name: string) {
-  if (isNil(name)) return
-  return storage?.removeItem(name)
-}
+  remove(name: string) {
+    if (isNil(name)) return
+    return storage?.removeItem(name)
+  },
 
-export function clearLocal() {
-  return storage?.clear()
+  clear() {
+    return storage?.clear()
+  }
 }
--- src/session.ts
+++ src/session.ts
@@ -2,27 +2,29 @@ import isNil from './_isNil'
 
 const storage = window?.sessionStorage
 
-export function hasSession(name: string) {
-  if (isNil(name)) return false
-  return !!storage?.getItem(name)
-}
-
-export function getSession(name: string) {
-  if (isNil(name)) return null
-  const result = storage?.getItem(name)
-  return result && JSON.parse(result)
-}
-
-export function setSession(name: string, value: string | number | boolean | Object) {
-  if (isNil(name) || isNil(value)) return
-  return storage?.setItem(name, JSON.stringify(value))
-}
-
-export function removeSession(name: string) {
-  if (isNil(name)) return
-  return storage?.removeItem(name)
-}
-
-export function clearSession() {
-  return storage?.clear()
+export default {
+  has(name: string) {
+    if (isNil(name)) return false
+    return !!storage?.getItem(name)
+  },
+  
+  get(name: string) {
+    if (isNil(name)) return null
+    const result = storage?.getItem(name)
+    return result && JSON.parse(result)
+  },
+  
+  set(name: string, value: string | number | boolean | Object) {
+    if (isNil(name) || isNil(value)) return
+    return storage?.setItem(name, JSON.stringify(value))
+  },
+  
+  remove(name: string) {
+    if (isNil(name)) return
+    return storage?.removeItem(name)
+  },
+  
+  clear() {
+    return storage?.clear()
+  }
 }
