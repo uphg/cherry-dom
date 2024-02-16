f2772c4cc012d24242671f142c30df2228703dd7
lvheng <1834805770@qq.com>
2024-02-16 22:19:12 +0800

    docs: add en/zh README, create vitepress document.

--- docs/.vuepress/client.ts
+++
@@ -1,9 +0,0 @@
-import { defineClientConfig } from '@vuepress/client'
-import Synta from './components/global/Synta.vue'
-
-export default defineClientConfig({
-  enhance({ app }) {
-    app.component('Synta', Synta)
-  },
-})
-
